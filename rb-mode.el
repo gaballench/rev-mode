5168af3ab439b9aa0e607f25f8965b5e02f543e6
gaballench <gaballench@gmail.com>
2018-04-20 00:23:36 -0300

    forget about the last commit, here run-rb works

--- rb-mode.el
+++ rb-mode.el
@@ -20,6 +20,67 @@
 ;; (add-hook 'sh-mode-hook 'rb-sh-hook)                             ;;
 ;; ------------------------------------------------------------------ ;;
 
+;;;;;;;;;;;;;;;;;;;;;;;;;
+; run-rb modified from run-julia in julia-mode. working properly
+;; Code for `inferior-rb-mode'
+(require 'comint)
+
+(defcustom rb-program "rb"
+  "Path to the program used by `inferior-rb'."
+  :type 'string
+  :group 'rb)
+
+(defcustom rb-arguments '()
+  "Commandline arguments to pass to `rb-program'."
+  :type 'string
+  :group 'rb)
+
+(defvar rb-prompt-regexp "^\\w*> "
+  "Regexp for matching `inferior-rb' prompt.")
+
+(defvar inferior-rb-mode-map
+  (let ((map (nconc (make-sparse-keymap) comint-mode-map)))
+    ;; example definition
+    (define-key map (kbd "TAB") 'rb-latexsub-or-indent)
+    map)
+  "Basic mode map for `inferior-rb-mode'.")
+
+;;;###autoload
+(defun inferior-rb ()
+    "Run an inferior instance of `rb' inside Emacs."
+    (interactive)
+    (let ((rb-program rb-program)
+          (buffer (get-buffer-create "*rb*")))
+      (when (not (comint-check-proc "*rb*"))
+            (apply #'make-comint-in-buffer "rb" "*rb*" rb-program rb-arguments))
+      (pop-to-buffer-same-window "*rb*")
+      (inferior-rb-mode)))
+
+(defun inferior-rb--initialize ()
+    "Helper function to initialize `inferior-rb'."
+    (setq comint-use-prompt-regexp t))
+
+(define-derived-mode inferior-rb-mode comint-mode "rb"
+  "Major mode for `inferior-rb'.
+
+\\<inferior-rb-mode-map>"
+  nil "rb"
+  (setq comint-prompt-regexp rb-prompt-regexp)
+  (setq comint-prompt-read-only t)
+  (set (make-local-variable 'font-lock-defaults) '(rb-font-lock-keywords t))
+  (set (make-local-variable 'paragraph-start) rb-prompt-regexp)
+  (set (make-local-variable 'indent-line-function) 'rb-indent-line))
+
+(add-hook 'inferior-rb-mode-hook 'inferior-rb--initialize)
+
+;;;###autoload
+(defalias 'run-rb #'inferior-rb
+  "Run an inferior instance of `rb' inside Emacs.")
+
+;(provide 'rb-mode)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
 ;; function taken from ess package
 (defun rb-next-code-line (&optional arg)
   "Move ARG lines of code forward (backward if ARG is negative).
@@ -40,48 +101,48 @@ On success, return 0.  Otherwise, go as far as possible and return -1."
     n))
 
 (defun process-rb ()
-  "returns a list with existing shell process."
+  "returns a list with existing rb process."
   (interactive)
   (setq listpr (process-list))
   (setq lengthpr (length listpr))
   (setq i 0)
-  (setq listshellp '())
+  (setq listrbp '())
   (while (< i lengthpr)
     (setq pos (string-match "rb" (prin1-to-string (elt listpr i))))
-    (if pos (add-to-list 'listshellp (process-name (get-process (elt listpr i)))))
+    (if pos (add-to-list 'listrbp (process-name (get-process (elt listpr i)))))
     (setq i (+ 1 i)))
-  listshellp)
+  listrbp)
 
 
-(defun process-shell-choose ()
+(defun process-rb-choose ()
   "returns which process to use."
 (interactive)
 (setq outpr 0)
 (setq cbuf (current-buffer))
-(setq shelllist (process-shell))
-(setq shelln (length shelllist))
-(if (eq shelln 0)
-    (progn (shell)
+(setq rblist (process-rb))
+(setq rbn (length rblist))
+(if (eq rbn 0)
+    (progn (rb)
 	   (switch-to-buffer cbuf)
 	   (setq outpr (get-process "rb"))
 	   (sleep-for 0.5)))
-(if (eq shelln 1)
-    (setq outpr (get-process (elt shelllist 0))))
-(if (> shelln 1)
+(if (eq rbn 1)
+    (setq outpr (get-process (elt rblist 0))))
+(if (> rbn 1)
 (progn
-(setq proc (completing-read "Send code to:" shelllist nil t (elt shelllist 0)))
+(setq proc (completing-read "Send code to:" rblist nil t (elt rblist 0)))
 (setq outpr (get-process proc))))
 outpr)
 
 
 (defun rb-eval-line (sprocess command)
-  "Evaluates a single command into the shell process."
+  "Evaluates a single command into the rb process."
   (setq sbuffer (process-buffer sprocess))
   (setq command (concat command "\n"))
   (accept-process-output sprocess 0 10)
   (with-current-buffer sbuffer 
     (end-of-buffer) ;point is not seen being moved (unless sbuffer is focused)
-    (insert command)			;pastes the command to shell
+    (insert command)			;pastes the command to rb
     (set-marker (process-mark sprocess) (point-max))
     (process-send-string sprocess command)
     ;; (accept-process-output sprocess 0 10)
@@ -89,57 +150,57 @@ outpr)
 
 ; maybe unnecessary? uncomment if needed 
 (defun rb-cd-current-directory ()
-  "Changes the shell working directory to the current buffer's one."
+  "Changes the rb working directory to the current buffer's one."
   (interactive)
-  (setq sprocess (process-shell-choose))
+  (setq sprocess (process-rb-choose))
   (setq com (format "cd %s" (file-name-directory default-directory)))
-  (shell-eval-line sprocess com))
+  (rb-eval-line sprocess com))
 
 
-(defun pipe-line-to-shell (&optional step)
-  "Evaluates the current line to the shell."
+(defun pipe-line-to-rb (&optional step)
+  "Evaluates the current line to the rb."
   (interactive ())
   (setq com (buffer-substring (point-at-bol) (point-at-eol)))
   (if (> (length com) 0)
       (progn
-	(setq sprocess (process-shell-choose))
-	(shell-eval-line sprocess com)
-	(when step (essh-next-code-line)))
+	(setq sprocess (process-rb-choose))
+	(rb-eval-line sprocess com)
+	(when step (rb-next-code-line)))
     (message "No command in this line")))
 
-(defun pipe-line-to-shell-and-step ()
-  "Evaluates the current line to the shell and goes to next line."
+(defun pipe-line-to-rb-and-step ()
+  "Evaluates the current line to the rb and goes to next line."
   (interactive)
-  (pipe-line-to-shell t))
+  (pipe-line-to-rb t))
 
-(defun pipe-region-to-shell (start end)
-  "Sends a region to the shell."
+(defun pipe-region-to-rb (start end)
+  "Sends a region to the rb."
   (interactive "r")
   (setq com (buffer-substring start end))	       ;reads command
   (setq lcom (length com))		       ;count chars
   (setq lastchar (substring com (1- lcom) lcom)) ;get last char
   (unless (string-match "\n" lastchar) ;if last char is not "\n", then...
     (setq com (concat com "\n")))	     ;...add it!
-  (setq sprocess (process-shell-choose))
+  (setq sprocess (process-rb-choose))
   (setq sbuffer (process-buffer sprocess))
   (while (> (length com) 0) 
     (setq pos (string-match "\n" com)) 
     (setq scom (substring com 0 pos))
     (setq com (substring com (min (length com) (1+ pos))))
-    (shell-eval-line sprocess scom)
+    (rb-eval-line sprocess scom)
     (accept-process-output sprocess 0 10)
     )) 
 
 
-(defun pipe-buffer-to-shell ()
-  "Evaluate whole buffer to the shell."
+(defun pipe-buffer-to-rb ()
+  "Evaluate whole buffer to the rb."
   (interactive)
-  (pipe-region-to-shell (point-min) (point-max)))
+  (pipe-region-to-rb (point-min) (point-max)))
 
-(defun pipe-function-to-shell ()
-"Evaluate function to the shell."
+(defun pipe-function-to-rb ()
+"Evaluate function to the rb."
 (interactive)
-(setq beg-end (essh-beg-end-of-function))
+(setq beg-end (rb-beg-end-of-function))
 (if beg-end
     (save-excursion
       (setq beg (nth 0 beg-end))
@@ -148,10 +209,10 @@ outpr)
       (setq origin (point-at-bol))
       (goto-line end)
       (setq terminal (point-at-eol))
-      (pipe-region-to-shell origin terminal))
+      (pipe-region-to-rb origin terminal))
   (message "No function at current point.")))
 
-(defun essh-beg-end-of-function ()
+(defun rb-beg-end-of-function ()
   "Returns the lines where the function starts and ends. If there is no function at current line, it returns nil."
   (interactive)
   (setq curline (line-number-at-pos))	;current line
@@ -176,5 +237,4 @@ outpr)
       (list beg end)
     nil))
   
-
-(provide 'essh)
+(provide 'rb-mode)
