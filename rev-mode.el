3669a61eb500fd5a238274cccd34c2d011522fc2
gaballench <gaballench@gmail.com>
2018-04-24 02:24:32 -0300

    Created a branch for the name change stuff. Failed to find a package called rev and therefore the original change to rb is unnecessary. Trying to revert breaks the code.

--- rev-mode.el
+++ rev-mode.el
@@ -1,113 +1,118 @@
 ;;; essh.el --- a set of commands that emulate for bash what ESS is to R.
 
-;; Filename: rb-mode.el
+;; Filename: rev-mode.el
 
 
 ;; ------------------------------------------------------------------ ;;
 ;; TO INSTALL:                                                        ;;
-;; 1. add rb-mode.el in your load-path.                                  ;;
+;; 1. add rev-mode.el in your load-path.                                  ;;
 ;;                                                                    ;;
 ;; 2. add to your .emacs file:                                        ;;
 ;;                                                                    ;;
-;; (require 'rb-mode)                                                    ;;
+;; (require 'rev-mode)                                                    ;;
 ;; (defun essh-sh-hook ()                                             ;;
-;;   (define-key sh-mode-map "\C-c\C-r" 'pipe-region-to-rb)        ;;
-;;   (define-key sh-mode-map "\C-c\C-b" 'pipe-buffer-to-rb)        ;;
-;;   (define-key sh-mode-map "\C-c\C-j" 'pipe-line-to-rb)          ;;
-;;   (define-key sh-mode-map "\C-c\C-n" 'pipe-line-to-rb-and-step) ;;
-;;   (define-key sh-mode-map "\C-c\C-f" 'pipe-function-to-rb)      ;;
-;;   (define-key sh-mode-map "\C-c\C-d" 'rb-cd-current-directory)) ;;
-;; (add-hook 'sh-mode-hook 'rb-sh-hook)                             ;;
+;;   (define-key sh-mode-map "\C-c\C-r" 'pipe-region-to-rev)        ;;
+;;   (define-key sh-mode-map "\C-c\C-b" 'pipe-buffer-to-rev)        ;;
+;;   (define-key sh-mode-map "\C-c\C-j" 'pipe-line-to-rev)          ;;
+;;   (define-key sh-mode-map "\C-c\C-n" 'pipe-line-to-rev-and-step) ;;
+;;   (define-key sh-mode-map "\C-c\C-f" 'pipe-function-to-rev)      ;;
+;;   (define-key sh-mode-map "\C-c\C-d" 'rev-cd-current-directory)) ;;
+;; (add-hook 'sh-mode-hook 'rev-sh-hook)                             ;;
 ;; ------------------------------------------------------------------ ;;
 
 ;;;;;;;;;;;;;; in .emacs:
-;;;; rb-mode will only run with these in the .emacs file
-;;;; and also runninng the following: opening the file, then run-rb
+;;;; rev-mode will only run with these in the .emacs file
+;;;; and also runninng the following: opening the file, then run-rev
 ;;;; this way, it "just works"
+;;;; If there is no rev process the first thing that happens is sh-mode opening a shell instance that fails to receive code from the keybindings
+;;;; However, if we open a run-rev process, code gets redirected properly
+;;;; So we have an unnecessary shell that causes no harm and a
+;;;; major mode that does not launch the right process when called by
+;;;; a file with proper extension. Still buggy but just works.
 ;(add-to-list 'load-path "/home/balleng/Dropbox/Gustavo/softwareDevel/rev-mode")
-;(require 'rb-mode)
-;(defun rb-mode-sh-hook ()                                             ;;
-;  (define-key sh-mode-map "\C-c\C-r" 'pipe-region-to-rb)        ;;
-;  (define-key sh-mode-map "\C-c\C-b" 'pipe-buffer-to-rb)        ;;
-;  (define-key sh-mode-map "\C-c\C-j" 'pipe-line-to-rb)          ;;
-;  (define-key sh-mode-map "\C-c\C-n" 'pipe-line-to-rb-and-step) ;;
-;  (define-key sh-mode-map "\C-c\C-f" 'pipe-function-to-rb))      ;;
-;;  (define-key sh-mode-map "\C-c\C-d" 'rb-cd-current-directory)) ;;
-;(add-hook 'sh-mode-hook 'rb-mode-sh-hook)
-;;; setup files ending in “.rev” to open in rb-mode
-;(add-to-list 'auto-mode-alist '("\\.rev\\'" . rb-mode))
+;(require 'rev-mode)
+;(defun rev-mode-sh-hook ()                                             ;;
+;  (define-key sh-mode-map "\C-c\C-r" 'pipe-region-to-rev)        ;;
+;  (define-key sh-mode-map "\C-c\C-b" 'pipe-buffer-to-rev)        ;;
+;  (define-key sh-mode-map "\C-c\C-j" 'pipe-line-to-rev)          ;;
+;  (define-key sh-mode-map "\C-c\C-n" 'pipe-line-to-rev-and-step) ;;
+;  (define-key sh-mode-map "\C-c\C-f" 'pipe-function-to-rev))      ;;
+;;  (define-key sh-mode-map "\C-c\C-d" 'rev-cd-current-directory)) ;;
+;(add-hook 'sh-mode-hook 'rev-mode-sh-hook)
+;;; setup files ending in “.rev” to open in rev-mode
+;(add-to-list 'auto-mode-alist '("\\.rev\\'" . rev-mode))
 ;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;; define autoload as an instance of sh-mode as suggested here http://ergoemacs.org/emacs/elisp_syntax_coloring.html
 ;;;###autoload
-(define-derived-mode rb-mode sh-mode "rb mode"
+(define-derived-mode rev-mode sh-mode "rev mode"
   "Major mode for editing and evaluating Rev")
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;
-; run-rb modified from run-julia in julia-mode. working properly
-;; Code for `inferior-rb-mode'
+; run-rev modified from run-julia in julia-mode. working properly
+;; Code for `inferior-rev-mode'
 (require 'comint)
 
-(defcustom rb-program "rb"
-  "Path to the program used by `inferior-rb'."
+(defcustom rev-program "rev"
+  "Path to the program used by `inferior-rev'."
   :type 'string
-  :group 'rb)
+  :group 'rev)
 
-(defcustom rb-arguments '()
-  "Commandline arguments to pass to `rb-program'."
+(defcustom rev-arguments '()
+  "Commandline arguments to pass to `rev-program'."
   :type 'string
-  :group 'rb)
+  :group 'rev)
 
-(defvar rb-prompt-regexp "^\\w*> "
-  "Regexp for matching `inferior-rb' prompt.")
+(defvar rev-prompt-regexp "^\\w*> "
+  "Regexp for matching `inferior-rev' prompt.")
 
-(defvar inferior-rb-mode-map
+(defvar inferior-rev-mode-map
   (let ((map (nconc (make-sparse-keymap) comint-mode-map)))
     ;; example definition
-    (define-key map (kbd "TAB") 'rb-latexsub-or-indent)
+    (define-key map (kbd "TAB") 'rev-latexsub-or-indent)
     map)
-  "Basic mode map for `inferior-rb-mode'.")
+  "Basic mode map for `inferior-rev-mode'.")
 
 ;;;###autoload
-(defun inferior-rb ()
-    "Run an inferior instance of `rb' inside Emacs."
+(defun inferior-rev ()
+    "Run an inferior instance of `rev' inside Emacs."
     (interactive)
-    (let ((rb-program rb-program)
-          (buffer (get-buffer-create "*rb*")))
-      (when (not (comint-check-proc "*rb*"))
-            (apply #'make-comint-in-buffer "rb" "*rb*" rb-program rb-arguments))
-      (pop-to-buffer-same-window "*rb*")
-      (inferior-rb-mode)))
+    (let ((rev-program rev-program)
+          (buffer (get-buffer-create "*rev*")))
+      (when (not (comint-check-proc "*rev*"))
+            (apply #'make-comint-in-buffer "rev" "*rev*" rev-program rev-arguments))
+      (pop-to-buffer-same-window "*rev*")
+      (inferior-rev-mode)))
 
-(defun inferior-rb--initialize ()
-    "Helper function to initialize `inferior-rb'."
+(defun inferior-rev--initialize ()
+    "Helper function to initialize `inferior-rev'."
     (setq comint-use-prompt-regexp t))
 
-(define-derived-mode inferior-rb-mode comint-mode "rb"
-  "Major mode for `inferior-rb'.
+(define-derived-mode inferior-rev-mode comint-mode "rev"
+  "Major mode for `inferior-rev'.
 
-\\<inferior-rb-mode-map>"
-  nil "rb"
-  (setq comint-prompt-regexp rb-prompt-regexp)
+\\<inferior-rev-mode-map>"
+  nil "rev"
+  (setq comint-prompt-regexp rev-prompt-regexp)
   (setq comint-prompt-read-only t)
-  (set (make-local-variable 'font-lock-defaults) '(rb-font-lock-keywords t))
-  (set (make-local-variable 'paragraph-start) rb-prompt-regexp)
-  (set (make-local-variable 'indent-line-function) 'rb-indent-line))
+  (set (make-local-variable 'font-lock-defaults) '(rev-font-lock-keywords t))
+  (set (make-local-variable 'paragraph-start) rev-prompt-regexp)
+  (set (make-local-variable 'indent-line-function) 'rev-indent-line))
 
-(add-hook 'inferior-rb-mode-hook 'inferior-rb--initialize)
+(add-hook 'inferior-rev-mode-hook 'inferior-rev--initialize)
 
 ;;;###autoload
-(defalias 'run-rb #'inferior-rb
-  "Run an inferior instance of `rb' inside Emacs.")
+(defalias 'run-rev #'inferior-rev
+  "Run an inferior instance of `rev' inside Emacs.")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;code modified from essh.el, it turns out to contain code that the developer of essh borrowed from sh-mode.el
 ;; function taken from ess package
-(defun rb-next-code-line (&optional arg)
+(defun rev-next-code-line (&optional arg)
   "Move ARG lines of code forward (backward if ARG is negative).
 Skips past all empty and comment lines.	 Default for ARG is 1.
 
@@ -125,7 +130,7 @@ On success, return 0.  Otherwise, go as far as possible and return -1."
       (setq arg (- arg inc)))
     n))
 
-(defun process-rb ()
+(defun process-rev ()
   "returns a list with existing shell process."
   (interactive)
   (setq listpr (process-list))
@@ -133,23 +138,23 @@ On success, return 0.  Otherwise, go as far as possible and return -1."
   (setq i 0)
   (setq listshellp '())
   (while (< i lengthpr)
-    (setq pos (string-match "rb" (prin1-to-string (elt listpr i))))
+    (setq pos (string-match "rev" (prin1-to-string (elt listpr i))))
     (if pos (add-to-list 'listshellp (process-name (get-process (elt listpr i)))))
     (setq i (+ 1 i)))
   listshellp)
 
 
-(defun process-rb-choose ()
+(defun process-rev-choose ()
   "returns which process to use."
 (interactive)
 (setq outpr 0)
 (setq cbuf (current-buffer))
-(setq shelllist (process-rb))
+(setq shelllist (process-rev))
 (setq shelln (length shelllist))
 (if (eq shelln 0)
     (progn (shell)
 	   (switch-to-buffer cbuf)
-	   (setq outpr (get-process "rb"))
+	   (setq outpr (get-process "rev"))
 	   (sleep-for 0.5)))
 (if (eq shelln 1)
     (setq outpr (get-process (elt shelllist 0))))
@@ -160,7 +165,7 @@ On success, return 0.  Otherwise, go as far as possible and return -1."
 outpr)
 
 
-(defun rb-eval-line (sprocess command)
+(defun rev-eval-line (sprocess command)
   "Evaluates a single command into the shell process."
   (setq sbuffer (process-buffer sprocess))
   (setq command (concat command "\n"))
@@ -182,48 +187,48 @@ outpr)
 ;  (shell-eval-line sprocess com))
 
 
-(defun pipe-line-to-rb (&optional step)
-  "Evaluates the current line to the rb interpreter."
+(defun pipe-line-to-rev (&optional step)
+  "Evaluates the current line to the rev interpreter."
   (interactive ())
   (setq com (buffer-substring (point-at-bol) (point-at-eol)))
   (if (> (length com) 0)
       (progn
-	(setq sprocess (process-rb-choose))
-	(rb-eval-line sprocess com)
-	(when step (rb-next-code-line)))
+	(setq sprocess (process-rev-choose))
+	(rev-eval-line sprocess com)
+	(when step (rev-next-code-line)))
     (message "No command in this line")))
 
-(defun pipe-line-to-rb-and-step ()
-  "Evaluates the current line to the rb interpreter and goes to next line."
+(defun pipe-line-to-rev-and-step ()
+  "Evaluates the current line to the rev interpreter and goes to next line."
   (interactive)
-  (pipe-line-to-rb t))
+  (pipe-line-to-rev t))
 
-(defun pipe-region-to-rb (start end)
-  "Sends a region to the rb interpreter."
+(defun pipe-region-to-rev (start end)
+  "Sends a region to the rev interpreter."
   (interactive "r")
   (setq com (buffer-substring start end))	       ;reads command
   (setq lcom (length com))		       ;count chars
   (setq lastchar (substring com (1- lcom) lcom)) ;get last char
   (unless (string-match "\n" lastchar) ;if last char is not "\n", then...
     (setq com (concat com "\n")))	     ;...add it!
-  (setq sprocess (process-rb-choose))
+  (setq sprocess (process-rev-choose))
   (setq sbuffer (process-buffer sprocess))
   (while (> (length com) 0) 
     (setq pos (string-match "\n" com)) 
     (setq scom (substring com 0 pos))
     (setq com (substring com (min (length com) (1+ pos))))
-    (rb-eval-line sprocess scom)
+    (rev-eval-line sprocess scom)
     (accept-process-output sprocess 0 10)
     )) 
 
 
-(defun pipe-buffer-to-rb ()
-  "Evaluate whole buffer to the rb interpreter."
+(defun pipe-buffer-to-rev ()
+  "Evaluate whole buffer to the rev interpreter."
   (interactive)
-  (pipe-region-to-rb (point-min) (point-max)))
+  (pipe-region-to-rev (point-min) (point-max)))
 
-(defun pipe-function-to-rb ()
-"Evaluate function to the rb interpreter."
+(defun pipe-function-to-rev ()
+"Evaluate function to the rev interpreter."
 (interactive)
 (setq beg-end (essh-beg-end-of-function))
 (if beg-end
@@ -234,10 +239,10 @@ outpr)
       (setq origin (point-at-bol))
       (goto-line end)
       (setq terminal (point-at-eol))
-      (pipe-region-to-rb origin terminal))
+      (pipe-region-to-rev origin terminal))
   (message "No function at current point.")))
 
-(defun rb-beg-end-of-function ()
+(defun rev-beg-end-of-function ()
   "Returns the lines where the function starts and ends. If there is no function at current line, it returns nil."
   (interactive)
   (setq curline (line-number-at-pos))	;current line
@@ -264,4 +269,4 @@ outpr)
 ;;;;;;;;;;;;;;
 
 
-(provide 'rb-mode)
+(provide 'rev-mode)
